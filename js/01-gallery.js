7a2423d5b4e4380c9aac7accc68f6da8917e9597
AlinaBielska <alinkabelka5@gmail.com>
2023-03-03 18:34:34 +0100

    dodano galerie obrazów

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,18 +3,30 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector(".gallery");
 
+//tworzenie galerii obrazków
 const allItems = galleryItems.map((galleryItem) => {
-    const item = `<div class="gallery_element">
-        <a class="gallery_link" href="${galleryItem.original}">
-        <img class="gallery-img" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}" />
+    const item = `<div class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
+        <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}" />
         </a>
         </div>`;
     return item; 
 })
     .join('');
 
+
+//dodano galerie obrazków
 gallery.innerHTML = allItems;
 
+
+// gallery.addEventListener("click") => {
+//     if (event.target.nodeName !== "IMG") {
+//         return;
+//     } {const instance = basicLightbox.create(`
+//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/flower-4202825_1280.jpg" width="800" height="600">`)
+
+// instance.show()}
+// }
 
 
 
