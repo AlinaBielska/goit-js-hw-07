3d799e2f3f6413ce411919a667fbe27ff1202a9d
AlinaBielska <alinkabelka5@gmail.com>
2023-03-03 17:23:03 +0100

    dodano galerie obrazków

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+const allItems = galleryItems.map((galleryItem) => {
+    const item = `<div class="gallery_element">
+        <a class="gallery_link" href="${galleryItem.original}">
+        <img class="gallery-img" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}" />
+        </a>
+        </div>`;
+    return item; 
+})
+    .join('');
+
+gallery.innerHTML = allItems;
+
+
+
+
 console.log(galleryItems);
